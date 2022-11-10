b90641cecf3a5ea2fefcb2080804a21d0a3477f4
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2022-11-10 14:31:28 -0700

    added consts to the index.js file

---
+++ index.js
@@ -0,0 +1,12 @@
+// The different types of portfiles
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+
+// Link to the create a new HTML
+const display = require('.//src/display');
+
+// Node modules
+const fs = require('fs');
+const inquirer = require('inquirer');
+
--- src/display.js
+++ src/display.js
@@ -101,7 +101,6 @@ function createTeam(data) {
         <head>
             <meta charset="UTF-8" />
             <link rel="stylesheet" href="./assets/css/style.css"/>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"/>
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
             <title>Team Profile</title>
         </head>
