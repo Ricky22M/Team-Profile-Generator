1126dc48e56ba83ffc48a8237485fb24e5578368
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2022-11-10 15:46:49 -0700

    added the code to _test_/employee.js

---
+++ _tests_/employee.test.js
@@ -0,0 +1,39 @@
+// Using the employee constructor
+const Employee = require('../lib/employee');
+
+// creates new employee object
+test('creates a new employee object', () => {
+    const employee = new Employee('Ricky', 45, 'rmedina2004@outlook.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+// gets the name from nameIS()
+test('gets employee name', () => {
+    const employee = new Employee('Ricky', 45, 'rmedina2004@outlook.com');
+
+    expect(employee.nameIs()).toEqual(expect.any(String));
+});
+
+// gets the ID from idIs()
+test('gets employee ID', () => {
+    const employee = new Employee('Ricky', 45, 'rmedina2004@outlook.com');
+
+    expect(employee.idIs()).toEqual(expect.any(Number));
+});
+
+// gets the email from emailIs()
+test('gets employee email', () => {
+    const employee = new Employee('Ricky', 45, 'rmedina2004@outlook.com');
+
+    expect(employee.emailIs()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+// gets the position from positionIs()
+test('gets the employee position', () => {
+    const employee = new Employee('Ricky', 45, 'rmedina2004@outlook.com');
+
+    expect(employee.positionIs()).toEqual("Employee");
+});
