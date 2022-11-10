4f694fbd39b75852cd26fc5d783d3286e765d6a3
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2022-11-10 14:54:17 -0700

    added an array and a type of employee function

--- index.js
+++ index.js
@@ -10,3 +10,32 @@ const display = require('.//src/display');
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+// Team 
+let Workforce = [];
+
+// To select the type of employee prompt
+function typeOfEmployee() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'position',
+            message: 'What type of employee will be added to your team?',
+            choices: ['Intern', 'Engineer', 'Manager', 'End my team selection'],
+        }
+    ]).then(function (input) {
+        switch(input.position) {
+            case 'Intern':
+                newIntern();
+                break;
+            case 'Engineer':
+                newEngineer();
+                break;
+            case 'Manager':
+                newManager();
+                break;
+            case 'End my team selection':
+                console.log('Newly created Team in new HTML file!');
+                break;
+        }
+    });
+}
